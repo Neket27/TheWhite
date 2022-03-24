3ecdc9152d12224735c4a1d11e933533dd19c578
Neket27 <96458390+Neket27@users.noreply.github.com>
2022-03-24 14:54:41 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-# Внимание!!!
-# Для запуска программы нужно прописать свои пути для файла "replacement.json" и где будет сохранён файл вывода данных(см. комент. ниже).
 import json
 import requests as requests
+def save_in_file(file_path,text_json):
+    for printt in text_json:
+        file = open(file_path, 'w')  # прописать свой путь для сохранения файла
+        with file as outfile:
+            json.dump(list_edited_text, outfile)
+    file.close()
+
+def get_list_dict_replacement_and_source(file_json_path):
+
+    with open(file_json_path) as json_file: # прописать сой путь к файлу "replacement.json"
+        data = json.load(json_file)
+
+    data = sorted(data, key=lambda d: d['replacement'])#сортировка ключей от меньшей длины к большей
+    data.reverse()
+    return data
+
 # Получение данных с API
 response = requests.get("https://raw.githubusercontent.com/thewhitesoft/student-2022-assignment/main/data.json")
-data_json = json.loads(response.text)
-
-text_data=data_json
-# Получение данных с файла
-# with open('C:/Users/nikit/Desktop/data.json') as json_file:
-#     text_data = json.load(json_file)
-
-with open('C:/Users/nikit/Desktop/replacement.json') as json_file: # прописать сой путь к файлу "replacement.json"
-    data = json.load(json_file)
-
-data = sorted(data, key=lambda d: d['replacement'])
-data.reverse()
-
+text_data = json.loads(response.text)
 list_edited_text=list(text_data)
+
 count=0
 edited_text=0
-for keys_data in data:
+for keys_data in get_list_dict_replacement_and_source('C:/Users/nikit/Desktop/replacement.json') :
     len_key=len(keys_data['replacement'])
     text_data.clear()
     text_data = list(list_edited_text)
@@ -28,7 +31,6 @@ for keys_data in data:
     count = 0
     replacement = (keys_data['replacement'])
     source = (keys_data['source'])
-
 
     while count < len(text_data):
         if str(replacement) in str(text_data[count]):
@@ -42,16 +44,7 @@ for keys_data in data:
                 list_edited_text[count] = edited_text
         count = count + 1
 
-
 list_edited_text=list(filter(None, list_edited_text))
-
-print("==================================================")
-for printt in list_edited_text:
-    print(printt)
-    file = open('C:/Users/nikit/Desktop/result.json', 'w')# прописать свой путь для сохранения файла
-    with file as outfile:
-        json.dump(list_edited_text, outfile)
-file.close()
-
+save_in_file('C:/Users/nikit/Desktop/result.json',list_edited_text)
 
 
