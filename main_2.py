5769325d513152c0df3045cb207c30a597d5b24f
Neket27 <96458390+Neket27@users.noreply.github.com>
2022-03-29 15:37:18 +1000

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -15,36 +15,43 @@ def get_list_dict_replacement_and_source(file_json_path):
     data = sorted(data, key=lambda d: d['replacement'])#сортировка ключей от меньшей длины к большей
     data.reverse()
     return data
-
 # Получение данных с API
 response = requests.get("https://raw.githubusercontent.com/thewhitesoft/student-2022-assignment/main/data.json")
 text_data = json.loads(response.text)
 list_edited_text=list(text_data)
 
-count=0
-edited_text=0
-for keys_data in get_list_dict_replacement_and_source('C:/Users/nikit/Desktop/replacement.json') :
-    len_key=len(keys_data['replacement'])
-    text_data.clear()
-    text_data = list(list_edited_text)
+def list_edited_text(list_edited_text,path):
+    count=0
+    edited_text='none'
+    for keys_data in get_list_dict_replacement_and_source(path) :
+        len_key=len(keys_data['replacement'])
+        text_data.clear()
+        text_data = list(list_edited_text)
 
-    count = 0
-    replacement = (keys_data['replacement'])
-    source = (keys_data['source'])
+        count = 0
+        replacement = (keys_data['replacement'])
+        source = (keys_data['source'])
 
-    while count < len(text_data):
-        if str(replacement) in str(text_data[count]):
+        while count < len(text_data):
+            if str(replacement) in str(text_data[count]):
 
-            if source != None:
-                if source != 'null':
-                    edited_text = text_data[count].replace(str(replacement), str(source))
+                if source != None:
+                    if source != 'null':
+                        edited_text = text_data[count].replace(str(replacement), str(source))
+                        list_edited_text[count] = edited_text
+                else:
+                    edited_text = text_data[count].replace(str(replacement), "")
                     list_edited_text[count] = edited_text
-            else:
-                edited_text = text_data[count].replace(str(replacement), "")
-                list_edited_text[count] = edited_text
-        count = count + 1
+            count = count + 1
 
-list_edited_text=list(filter(None, list_edited_text))
-save_in_file('C:/Users/nikit/Desktop/result.json',list_edited_text)
+    list_edited_text=list(filter(None, list_edited_text))
+    return list_edited_text
+
+
+
+
+
+text=list_edited_text(list_edited_text,'C:/Users/nikit/Desktop/replacement.json')
+save_in_file('C:/Users/nikit/Desktop/result.json',text)
 
 
